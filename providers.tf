dba405700c716eb377dab0432ffbfa6b77c9a2fa
Hinasjamil <126811921+Hinasjamil@users.noreply.github.com>
2026-02-02 00:20:33 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -12,12 +12,10 @@ terraform {
 }
 
 provider "azurerm" {
-   subscription_id = "bb46ce64-f409-4c00-8231-f5e3a23cae88"
- tenant_id = "7e368038-1167-4dbb-869b-098e9a100f09"
- client_id = "e6dc1c46-7c62-4c71-8419-1a5aeb993af9"
- client_secret = "tlx8Q~YfyRbTgTcYa8MsdIRRFTSD.uX6v0o8Sa4O"
+
 
 
 
    features {}
 }
+
